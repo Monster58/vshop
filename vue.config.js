38aeb17dfaca5c6cf9777f32765b341a4cb1aa09
dududu <dadada@gmail.comm>
2019-11-07 17:20:11 +0800

    配置代理解决跨域

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 // vue.config.js
 module.exports = {
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4000',
-                ws: true,
-                changeOrigin: true
-            }
-        }
+  devServer: {
+    port: "3000", //前台代理端口
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        ws: true, //如果要代理websockets
+        changeOrigin: true //将选项changeOrigin设置true为基于名称的虚拟托管站点。
+      }
     }
-}
+  }
+};
